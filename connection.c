06c239d77374bdfb9ee69cbe19264c48047f7820
chad-iris <chad.trabant@iris.edu>
2020-04-17 18:14:58 -0700

    Use SCNd64 to properly sscanf() int64_t types

--- connection.c
+++ connection.c
@@ -1400,7 +1400,7 @@ dl_handlereply (DLCP *dlconn, void *buffer, int buflen, int64_t *value)
   cbuffer[buflen] = '\0';
 
   /* Parse reply header */
-  if (sscanf (buffer, "%10s %lld %lld", status, &pvalue, &size) != 3)
+  if (sscanf (buffer, "%10s %" SCNd64 " %" SCNd64, status, &pvalue, &size) != 3)
   {
     dl_log_r (dlconn, 2, 0, "[%s] dl_handlereply(): Unable to parse reply header: '%s'\n",
               dlconn->addr, (char *)buffer);
